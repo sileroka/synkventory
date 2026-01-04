5bea5bfa8d8006cfc4ebdace52d37f2f5bd66fca
Sean <seanrh13@gmail.com>
2026-01-04 07:56:41 -0800

    fix: correct migration down_revision to reference 0008 instead of timestamp

--- backend/alembic/versions/20260104_030000_add_image_key_to_inventory.py
+++ backend/alembic/versions/20260104_030000_add_image_key_to_inventory.py
@@ -1,7 +1,7 @@
 """Add image_key to inventory_items
 
 Revision ID: 20260104_030000
-Revises: 20260104_020000
+Revises: 0008
 Create Date: 2026-01-04 03:00:00.000000
 
 """
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "20260104_030000"
-down_revision: Union[str, None] = "20260104_020000"
+down_revision: Union[str, None] = "0008"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
